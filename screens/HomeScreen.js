e1df4a277e6c4955ba8935af63dc15fbfe182d98
bjamyl <alhassanjamil0@gmail.com>
2022-07-21 16:28:54 +0000

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,53 @@
+import { Image, ScrollView, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  AdjustmentsIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
+
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-5 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00ccbb" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00ccbb" />
+      </View>
+      {/* Search*/}
+      <View className="flex-row items-center space-x-2 pb-2 mx-2">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <SearchIcon color="gray" />
+          <TextInput placeholder="Restaurants and cuisines" />
+        </View>
+        <AdjustmentsIcon color="#00ccbb" />
+      </View>
+      {/*Body */}
+      <ScrollView>
+        
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
